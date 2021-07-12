2373a570a7c2d55d00bdac974c75f6bc47c1b228
my-acct-cypher <87336431+my-acct-cypher@users.noreply.github.com>
2021-07-12 22:33:34 +0000

    Create jones.java
    this is to certify contribution to the branch checks out fine on execution

---
+++ jones.java
@@ -0,0 +1,22 @@
+   private List<RestaurantModel> getRestaurantData() {
+        InputStream is = getResources().openRawResource(R.raw.restaurent);
+        Writer writer = new StringWriter();
+        char[] buffer = new char[1024];
+        try{
+            Reader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+            int n;
+            while(( n = reader.read(buffer)) != -1) {
+                writer.write(buffer, 0,n);
+            }
+        }catch (Exception e) {
+
+        }
+
+        String jsonStr = writer.toString();
+        Gson gson = new Gson();
+        RestaurantModel[] restaurantModels =  gson.fromJson(jsonStr, RestaurantModel[].class);
+        List<RestaurantModel> restList = Arrays.asList(restaurantModels);
+
+        return  restList;
+
+    }
